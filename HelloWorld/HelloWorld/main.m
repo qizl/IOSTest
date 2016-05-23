2b13ce1d194b2bb6b68087547903f71efdfb8475
qizl <q@qizl.cn>
2016-05-23 15:08:30 +0800

    -

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by qizl on 16/5/20.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
