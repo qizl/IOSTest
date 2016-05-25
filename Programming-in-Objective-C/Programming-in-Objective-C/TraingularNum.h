d4371abe133aa0b9d38e1e24d313f1b7665912e9
qizl <q@qizl.cn>
2016-05-26 00:11:10 +0800

    -

---
+++ Programming-in-Objective-C/Programming-in-Objective-C/TraingularNum.h
@@ -0,0 +1,13 @@
+//
+//  TraingularNum.h
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/5/25.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#ifndef TraingularNum_h
+#define TraingularNum_h
+
+
+#endif /* TraingularNum_h */
---
+++ Programming-in-Objective-C/Programming-in-Objective-C/TraingularNum.m
@@ -0,0 +1,9 @@
+//
+//  TraingularNum.m
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/5/25.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
