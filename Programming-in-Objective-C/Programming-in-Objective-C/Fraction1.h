b5632789cceda66923bbd0df44215f668e8da0ba
qizl <q@qizl.cn>
2016-06-01 00:53:56 +0800

    -

---
+++ Programming-in-Objective-C/Programming-in-Objective-C/Fraction1.h
@@ -0,0 +1,13 @@
+//
+//  Fraction1.h
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/6/1.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#ifndef Fraction1_h
+#define Fraction1_h
+
+
+#endif /* Fraction1_h */
---
+++ Programming-in-Objective-C/Programming-in-Objective-C/Fraction1.m
@@ -0,0 +1,9 @@
+//
+//  Fraction1.m
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/6/1.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
