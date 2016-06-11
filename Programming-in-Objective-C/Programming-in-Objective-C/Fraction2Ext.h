9b2544eef476c4e91328fb916e1473383c8093cc
qizl <q@qizl.cn>
2016-06-11 17:52:27 +0800

    -

---
+++ Programming-in-Objective-C/Programming-in-Objective-C/Fraction2Ext.h
@@ -0,0 +1,13 @@
+//
+//  Fraction2Ext.h
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/6/11.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#ifndef Fraction2Ext_h
+#define Fraction2Ext_h
+
+
+#endif /* Fraction2Ext_h */
---
+++ Programming-in-Objective-C/Programming-in-Objective-C/Fraction2Ext.m
@@ -0,0 +1,9 @@
+//
+//  Fraction2Ext.m
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/6/11.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
