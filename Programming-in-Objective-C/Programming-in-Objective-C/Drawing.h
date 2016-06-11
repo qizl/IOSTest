da67c714a276fa61b663581bad286d81b7ced297
qizl <q@qizl.cn>
2016-06-11 23:04:43 +0800

    -

---
+++ Programming-in-Objective-C/Programming-in-Objective-C/Drawing.h
@@ -0,0 +1,13 @@
+//
+//  Drawing.h
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/6/11.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#ifndef Drawing_h
+#define Drawing_h
+
+
+#endif /* Drawing_h */
---
+++ Programming-in-Objective-C/Programming-in-Objective-C/GraphicObject.h
@@ -0,0 +1,13 @@
+//
+//  GraphicObject.h
+//  Programming-in-Objective-C
+//
+//  Created by qizl on 16/6/11.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#ifndef GraphicObject_h
+#define GraphicObject_h
+
+
+#endif /* GraphicObject_h */
