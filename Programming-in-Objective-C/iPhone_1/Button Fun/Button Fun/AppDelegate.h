df2970150b0a0b48c2285561d840f37b6bd72651
qizl <q@qizl.cn>
2016-06-22 21:48:18 +0800

    -

---
+++ Programming-in-Objective-C/iPhone_1/Button Fun/Button Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Button Fun
+//
+//  Created by qizl on 16/6/22.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Programming-in-Objective-C/iPhone_1/Button Fun/Button Fun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Button Fun
+//
+//  Created by qizl on 16/6/22.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
